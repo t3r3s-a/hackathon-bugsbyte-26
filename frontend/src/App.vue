a915e94e5e414a013785b342d7f082bf0b0771ac
a111415 <a111415@alunos.uminho.pt>
2026-02-14 00:30:35 +0000

    feat: initial structure with login view added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginView from './views/loginView.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <img alt="nutrium logo" src="./assets/logo-nutrium.png" class="logo">
+    <LoginView/>
+
 </template>
 
 <style scoped>
+
 .logo {
-  height: 6em;
+  height: 100px;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
